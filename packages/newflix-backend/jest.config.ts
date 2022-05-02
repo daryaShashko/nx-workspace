6178ef7fcb5c0323fbf810fa754ecfdf55760ae7
Darya Shashko <darya_shashko@epam.com>
2022-05-02 14:50:20 +0300

    chore(newflix-backend): added nest to the ws

---
+++ packages/newflix-backend/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'newflix-backend',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/newflix-backend',
+};
