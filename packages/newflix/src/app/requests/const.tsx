06ae73648b258ba7a9778224cd25899bc9653113
Darya Shashko <darya_shashko@epam.com>
2022-07-18 17:44:18 +0300

    feat(fe and be): change build files

--- packages/newflix/src/app/requests/const.tsx
+++ packages/newflix/src/app/requests/const.tsx
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3333';
+// export const API_BASE_URL = 'http://localhost:3333';
+export const API_BASE_URL = '/';
 export const API_PREFIX = 'api';
 
 export const FILMS_URL = `${API_BASE_URL}/${API_PREFIX}/films`;
